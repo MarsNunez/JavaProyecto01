72e755def50978fc9ba24accfc29e98fb3fe1f25
NASebastian <na.marcelo.sebastian@gmail.com>
2022-08-22 08:11:38 -0500

    Colocando codigo inicial

---
+++ Principal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		int vida, rp;
+		boolean fl = false;
+		vida = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			vida ++;
+			System.out.println("Vida: " + vida + ": Cual es el numero secreo?");
+			rp = sc.nextInt();
+			if (rp == 1234)
+				fl = true;
+		} while (vida < 3 && !fl );
+		if (fl == true) {
+			System.out.println("Adivinaste!!!");
+			
+		} else {
+			System.out.println("Ups... Perdiste.");
+		}
+		sc.close();
+	}
+}
+
+
